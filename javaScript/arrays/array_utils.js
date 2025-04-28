6f4a6129566c964a8ec5c16a6b5a5f56e4b8febd
Elena <elena.wictorovna@gmail.com>
2025-04-28 14:35:03 +0300

    Update array_utils.js

--- javaScript/arrays/array_utils.js
+++ javaScript/arrays/array_utils.js
@@ -8,3 +8,34 @@ function deleteAllDublicates (array) {
 }
 
 
+/**
+ * возвращает случайный элемент из массива
+ * @param {Array} array - массив с элементами
+ * @returns {*} -случайный элемент массива или 'null', если массив пуст или не является массивом
+ * @example
+ * getRandomElement([1, 2, 3]); // 2 (случайный элемент)
+ * getRandomElement([]); // null 
+ */
+function getRandomElement (arr) {
+    if (arr.length === 0 || !Array.isArray(arr)) return null;
+
+    let max = arr.length - 1;
+    let min = 0;
+
+    let randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+    return arr[randomInt];
+}
+
+/**
+ * возвращает случайный элемент из массива (без проверки на пустоту), более короткий вариант, чем выше
+ * @param {Array} array - массив с элементами
+ * @returns {*} -случайный элемент массива или 'null', если массив пуст или не является массивом
+ * @example
+ * getRandomElement([1, 2, 3]); // 2 (случайный элемент)
+ */
+
+function getRandomElementShort (arr) {
+    return arr[Math.floor(Math.rendom() * arr.length)];
+}
