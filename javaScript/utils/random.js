27ea2ce55c2b5ea0e19982f69ae22a0616811fff
Elena <elena.wictorovna@gmail.com>
2025-05-06 16:48:43 +0300

    Update random.js

--- javaScript/utils/random.js
+++ javaScript/utils/random.js
@@ -10,3 +10,28 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * Генерирует случайный цвет в RGB-формате (rgb(r, g, b))
+ * @returns {string} случайный RGB-цвет, например "rgb(100, 200, 50)"
+ */
+function getRandonRGBColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+/**
+ * Генерирует случайный цвет в HEX формате (#RRGGBB)
+ * @returns {string} случайный HEX-цвет, например "A1B2C3"
+ */
+function getRandomColor () {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
