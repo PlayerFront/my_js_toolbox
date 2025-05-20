a6bafc4296c5d186fa1706ff53d43a716929f8a0
Elena <elena.wictorovna@gmail.com>
2025-05-20 17:57:28 +0300

    Create sum.js

---
+++ javaScript/utils/sum.js
@@ -0,0 +1,8 @@
+/**
+ * Возвращает сумму чисел от 1 до N
+ * @param {number} n - максимальное число в последовательности
+ * @returns {number}
+ */
+function sumNumbers (n){
+    return n * (n + 1) / 2;
+}
